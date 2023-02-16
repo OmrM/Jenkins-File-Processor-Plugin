5b86c3ed25fafd6c404a25e8d00a717600fda1aa
OmrM <omar12726@gmail.com>
2023-02-16 02:03:03 -0700

    added skeleton for new builder class, action class, and jelly xml

---
+++ file_detector/src/main/java/io/jenkins/plugins/sample/FileProcessingAction.java
@@ -0,0 +1,56 @@
+/*HelloWorld Action  */
+
+package io.jenkins.plugins.sample;
+import hudson.model.Run;
+import jenkins.model.RunAction2;
+public class FileProcessingAction implements RunAction2 {
+    private transient Run run;
+    @Override
+    public void onAttached(Run<?, ?> run) {
+        this.run = run;
+    }
+
+    @Override
+    public void onLoad(Run<?, ?> run) {
+        this.run = run;
+    }
+
+    public Run getRun() {
+        return run;
+    }
+    //With these changes, the action Display Name will show in the build’s side panel, and link to the URL \http://JENKINS/job/JOBNAME/BUILDNUMBER/greeting/.
+    //the page at that url needs to be defined with apache commons Jelly
+    @Override
+    public String getIconFileName() {
+        //This is the icon used for the side panel item. document.png is one of the predefined icons bundled with Jenkins.
+        return "document.png";
+    }
+
+    @Override
+    public String getDisplayName() {
+        //This is the label used for the side panel item.
+        return "Files";
+    }
+
+    @Override
+    public String getUrlName() {
+        //This is the URL fragment used for this action.
+        return "files";
+    }
+
+
+
+    private String name;
+    //constructor that takes in name as an arg
+    public FileProcessingAction() {
+        //this.name = name;
+    }
+    //getter for the name
+    public String getName() {
+        return name;
+    }
+
+
+
+
+}
--- file_detector/src/main/java/io/jenkins/plugins/sample/FileProcessingBuilder.java
+++ file_detector/src/main/java/io/jenkins/plugins/sample/FileProcessingBuilder.java
@@ -19,25 +19,25 @@ import jenkins.tasks.SimpleBuildStep;
 import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.DataBoundSetter;
 
-public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
-    //variables for Parameters: name, userFrench
+public class FileProcessingBuilder extends Builder implements SimpleBuildStep {
+
+    //Getters and setters for Parameters: name, userFrench
     private final String name;
     private boolean useFrench;
 
-    //constructor for the input parameter "name"
     @DataBoundConstructor
-    public HelloWorldBuilder(String name) {
+    public FileProcessingBuilder(String name) {
         this.name = name;
     }
-    //getter for paramter "name"
+
     public String getName() {
         return name;
     }
-    //getter for useFrench boolean
+
     public boolean isUseFrench() {
         return useFrench;
     }
-    //setter that checks for useFrench parameter
+
     @DataBoundSetter
     public void setUseFrench(boolean useFrench) {
         this.useFrench = useFrench;
@@ -49,28 +49,14 @@ public class HelloWorldBuilder extends Builder implements SimpleBuildStep {
         //calling the HelloWorldAction and passing in the name. from "Extend the Plugin" docs:
         run.addAction(new HelloWorldAction(name));
 
-        if (useFrench) {
-            listener.getLogger().println("Bonjour, " + name + "!");
-        } else {
-            listener.getLogger().println("Hello, " + name + "!");
-        }
+        run.addAction(new FileProcessingAction());
     }
 
     @Symbol("greet")
     @Extension
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
 
-        public FormValidation doCheckName(@QueryParameter String value, @QueryParameter boolean useFrench)
-                throws IOException, ServletException {
-            if (value.length() == 0)
-                return FormValidation.error(Messages.HelloWorldBuilder_DescriptorImpl_errors_missingName());
-            if (value.length() < 4)
-                return FormValidation.warning(Messages.HelloWorldBuilder_DescriptorImpl_warnings_tooShort());
-            if (!useFrench && value.matches(".*[éáàç].*")) {
-                return FormValidation.warning(Messages.HelloWorldBuilder_DescriptorImpl_warnings_reallyFrench());
-            }
-            return FormValidation.ok();
-        }
+
 
         @Override
         public boolean isApplicable(Class<? extends AbstractProject> aClass) {
